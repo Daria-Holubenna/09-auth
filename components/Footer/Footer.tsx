5970a09935e2385356009676151a4bd3528b3e4d
Daria_H <dholubenna@gmail.com>
2025-08-27 23:53:52 +0300

    fex middleware

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Oleh Nezveshchuk</p>
+          <p>Developer: DARIA_H</p>
           <p>
             Contact us:
             <Link href="mailto:student@notehub.app">student@notehub.app</Link>
